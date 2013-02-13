5e4ebf75fcb567460de9d138ea157d9ccebbe5f1
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2013-02-13 18:12:47 +0100

    add regions with carmen

--- lib/generators/installs/installs_generator.rb
+++ lib/generators/installs/installs_generator.rb
@@ -7,9 +7,14 @@ class InstallsGenerator < Rails::Generators::Base
 
   def setup_routes
     template "controllers/countries_controller.rb", "app/controllers/countries_controller.rb"
+    template "controllers/regions_controller.rb", "app/controllers/regions_controller.rb"
     template "models/country.rb", "app/models/country.rb"
+    template "models/region.rb", "app/models/region.rb"
     template "config/initializers/carmen.rb", "config/initializers/carmen.rb"
-    route_carmen_restfull = ":countries, :except => [:show, :new, :edit, :create, :update, :destroy]"
+    route_carmen_restfull = "resources  :countries, :except => [:show, :new, :edit, :create, :update, :destroy] do "
+    route_carmen_restfull += "  resources :regions, :except => [:edit, :update, :destroy, :new, :show, :index, :create]     "
+    route_carmen_restfull += "end"
+    route_carmen_restfull += "match 'countries/:country_name/regions' => 'regions#index'"
     route route_carmen_restfull
   end
 end
